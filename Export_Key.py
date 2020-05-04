6be8bc3f1b5195a845c06b606b6a2e58ab9b32ae
Tahir Temaj <tahir1temaj@gmail.com>
2020-05-04 22:51:04 +0200

    Add files via upload

--- Export_Key.py
+++ Export_Key.py
@@ -2,7 +2,7 @@ import sys
 
 
 def main():
-    print("-----------------------------------------------------------------------------\n")
+    print("-----------------------------------------------------------------------------\nVendos Komanden: ")
     inputi = input()
     komanda = inputi.split()
 
@@ -10,28 +10,45 @@ def main():
         vlera = komanda[0]
         lloji = komanda[1]
         emri = komanda[2]
-        ruaj = komanda[3]+".xml"
+        try:
+            ruaj = komanda[3]+".xml"
+        except:
+            print("")
 
 
         if vlera.upper() == "EXPORT-KEY":
             if lloji.upper() == "PUBLIC":
                 emri1 = emri + ".pub.xml"
-                file = open("Keys/"+ emri1,"r")
-                if file.mode == "r":
-                    permbajtja = file.read()
-                    f = open(ruaj,"a+")
-                    f.write(permbajtja)
-                    print("Qelesi publik u ruajt ne fajllin "+ruaj)
+                try:
+                    file = open("Keys/"+ emri1,"r")
+                    if file.mode == "r":
+                        permbajtja = file.read()
+                        try:
+                            f = open(ruaj,"a+")
+                            f.write(permbajtja)
+                            print("Qelesi publik u ruajt ne fajllin "+ruaj)
+                            Provo()
+                        except:
+                                print(permbajtja)
+                except:
+                    print("Gabim: Qelesi publik "+emri1+" nuk ekziston")
                     Provo()
 
             elif lloji.upper() == "PRIVATE":
                 emri1 = emri + ".xml"
-                file = open("Keys/" + emri1, "r")
-                if file.mode == "r":
-                    permbajtja = file.read()
-                    f = open(ruaj, "a+")
-                    f.write(permbajtja)
-                    print("Qelesi privat u ruajt ne fajllin " + ruaj)
+                try:
+                    file = open("Keys/" + emri1, "r")
+                    if file.mode == "r":
+                        permbajtja = file.read()
+                        try:
+                            f = open(ruaj, "a+")
+                            f.write(permbajtja)
+                            print("Qelesi privat u ruajt ne fajllin " + ruaj)
+                            Provo()
+                        except:
+                            print(permbajtja)
+                except:
+                    print("Gabim: Qelesi privat "+emri1+" nuk ekziston")
                     Provo()
         else:
             print("Gabim ju lutem vendosni komanda valide shemb(export-key public filan fiskteku)\n")
