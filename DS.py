ad4639e857f9fbf2c7b8f6af23ba9a042c996b99
Tahir Temaj <tahir1temaj@gmail.com>
2020-05-04 22:51:31 +0200

    Add files via upload

--- DS.py
+++ DS.py
@@ -2,25 +2,38 @@ import Four_square_cipher
 import Vigenere
 import sys
 import Case
+import Create_user
+import Export_Key
+
+
+def Provo():
+    zgjedhja = input("Deshiron te provoshe perser(PO/JO):")
+    if zgjedhja.upper() == "PO":
+        main()
+    else:
+        print("----Procesi perfundoi-----")
+
 
 def main():
     print("Sheno cilin fajll do te ekzekutosh: ")
     sys.argv = input("vendos emrin e fajllit:").upper()
     if sys.argv == "VIGENERE" :
         Vigenere.main()
-
+        Provo()
     elif sys.argv == "FOUR_SQUARE_CIPHER":
         Four_square_cipher.main()
+        Provo()
+    elif sys.argv.upper() == "CREATE_USER":
+        Create_user.main()
+        Provo()
+
+    elif sys.argv.upper() == "EXPORT_KEY":
+        Export_Key.main()
+        Provo()
 
     elif sys.argv == "CASE":
         Case.main()
-
-    x = input("******Deshiron te provosh perseri: ").upper()
-    if x == 'YES':
-        main()
-    elif x == 'NO':
-        print("******Processi perfundoi*******")
-        exit()
+        Provo()
 
     else:
         print("Vendos vlera valide ")
@@ -28,4 +41,3 @@ def main():
 
 if __name__ == "__main__":
     main()
-
